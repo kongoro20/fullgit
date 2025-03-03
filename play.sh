00bbbe9410f6162f4a1de56e6fad4bd45eb0a01a
kongoro20 <kongo6411@gmail.com>
2025-03-03 03:01:13 +0100

    Update play.sh

--- play.sh
+++ play.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
+bash setup.sh
 
+# Activate the Python virtual environment
+source ./myenv/bin/activate
+
+# Check if activation was successful
+if [ $? -ne 0 ]; then
+  echo "Failed to activate the virtual environment."
+  exit 1
+fi
+
+# Prepare environment
+touch ~/.Xauthority
+
+# Install required Python packages
+pip install pyautogui
+pip install --upgrade pillow
+pip install opencv-python-headless
+pip install pyperclip
+sleep 2
 bash script1.sh
 sleep 2
 bash run.sh
