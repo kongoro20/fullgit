73fe1b9db6eb322a228bd2a77d0e28f5e321c66d
kongoro20 <kongo6411@gmail.com>
2025-03-28 02:16:09 +0100

    Update replay.sh

--- replay.sh
+++ replay.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
+
 sleep 1
-source myenv/bin/activate
+source /root/myenv/bin/activate
 sleep 2
 
+echo "Starting open.sh..." >> /root/replay_out.log
 bash open.sh
 if [ $? -ne 0 ]; then
-  echo "open.sh failed, exiting..."
-  exit 1
+    echo "open.sh failed, exiting..." | tee -a /root/replay_err.log
+    exit 1
 fi
 sleep 3
 
+echo "Starting run.sh..." >> /root/replay_out.log
 bash run.sh
 if [ $? -ne 0 ]; then
-  echo "setup.sh failed, exiting..."
-  exit 1
+    echo "run.sh failed, exiting..." | tee -a /root/replay_err.log
+    exit 1
 fi
+
+echo "replay.sh completed successfully." >> /root/replay_out.log
